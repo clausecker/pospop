3869f61c26ff88dcdbf4b50342b69142e1204125
Robert Clausecker <fuz@fuz.su>
2020-08-19 16:39:24 +0200

    add count16 benchmarks
    First step in actually adding count16 kernels

--- bench_test.go
+++ bench_test.go
@@ -13,11 +13,7 @@ var benchmarkLengths = []int{
 var benchmarkLengthsShort = []int{100 * 1000}
 
 // benchmark a count8 implementation
-func benchmarkCount8(b *testing.B, lengths []int, count8 func(*[8]int, []uint8)) {
-	maxlen := lengths[len(lengths)-1]
-	buf := make([]uint8, maxlen)
-	rand.Read(buf)
-
+func benchmarkCount8(b *testing.B, buf []uint8, lengths []int, count8 func(*[8]int, []uint8)) {
 	for _, l := range lengths {
 		b.Run(strconv.Itoa(l), func(b *testing.B) {
 			var counts [8]int
@@ -42,13 +38,60 @@ func BenchmarkCount8(b *testing.B) {
 		lengths = benchmarkLengthsShort
 	}
 
+	maxlen := lengths[len(lengths)-1]
+	buf := make([]uint8, maxlen)
+	rand.Read(buf)
+
 	for _, impl := range funcs {
 		b.Run(impl.name, func(bb *testing.B) {
 			if !impl.available {
 				bb.SkipNow()
 			}
 
-			benchmarkCount8(bb, lengths, impl.count8)
+			benchmarkCount8(bb, buf, lengths, impl.count8)
+		})
+	}
+}
+
+// benchmark a count16 implementation
+func benchmarkCount16(b *testing.B, buf []uint16, lengths []int, count16 func(*[16]int, []uint16)) {
+	for _, l := range lengths {
+		b.Run(strconv.Itoa(l), func(b *testing.B) {
+			var counts [16]int
+			testbuf := buf[:l]
+			b.SetBytes(int64(l))
+			for i := 0; i < b.N; i++ {
+				count16(&counts, testbuf)
+			}
+		})
+	}
+}
+
+// benchmark all Count16 implementations
+func BenchmarkCount16(b *testing.B) {
+	funcs := count16funcs
+	lengths := benchmarkLengths
+
+	// short benchmark: only test the implementation
+	// actually used and keep it to one size
+	if testing.Short() {
+		funcs = []count16impl{{Count16, "dispatch", true}}
+		lengths = benchmarkLengthsShort
+	}
+
+	maxlen := lengths[len(lengths)-1]
+	buf := make([]uint16, maxlen)
+	for i := range buf {
+		buf[i] = uint16(rand.Int63())
+	}
+
+	for _, impl := range funcs {
+		b.Run(impl.name, func(bb *testing.B) {
+			if !impl.available {
+				bb.SkipNow()
+			}
+
+			benchmarkCount16(bb, buf, lengths, impl.count16)
 		})
 	}
 }
