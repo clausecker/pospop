d24d616b79e1661782087f4ac77ba98aa7d55e58
Robert Clausecker <fuz@fuz.su>
2020-08-25 12:47:20 +0200

    Add new count8 variant using bit matrix transpositions
    This one performs slightly worse than the other AVX2 variant
    but will be useful for count16 and higher as arbitrary count
    widths can be generated simply by reducing the results less.

--- count8avx2var_amd64.s
+++ count8avx2var_amd64.s
@@ -1,10 +1,14 @@
+// +build ignore
+
 #include "textflag.h"
 
 // 8 bit positional population count using AVX-2.
 // Processes 480 bytes at a time using a 15-fold
-// carry-save-adder reduction.  The bits are then
-// grouped with VPMOVMSKB and counted individually
-// with POPCNT.  Required feature flags: POPCNT, AVX2
+// carry-save-adder reduction followed by a series
+// of bit matrix transpositions.  This kernel is
+// not used as it is slightly slower than the
+// VPMOVMSKB variant.
+// Required feature flags: AVX2
 
 // B:A = A+B+C, D used for scratch space
 #define CSA(A, B, C, D) \
@@ -24,28 +28,28 @@
 	COUNT(Y, R) \
 	VPADDB Y, Y, Y
 
-// count the number of MSB set in Y4:Y3:Y1:Y0
-// and accumulate into R
-#define ACCUM(R) \
-	COUNT(Y4, AX) \
-	COUNT(Y3, BX) \
-	LEAL (BX)(AX*2), AX \
-	COUNT(Y1, BX) \
-	COUNT(Y0, DX) \
-	LEAL (DX)(BX*2), BX \
-	LEAL (BX)(AX*4), AX \
-	ADDQ AX, R
+// magic transposition constants
+DATA magic<>+0(SB)/4, $0x00aa00aa
+DATA magic<>+4(SB)/4, $0x0000cccc
+DATA magic<>+8(SB)/4, $0x0f0f0f0f
+GLOBL magic<>(SB), RODATA|NOPTR, $12
 
-// same as ACCUM, but use COUNTS instead of COUNT
-#define ACCUMS(R) \
-	COUNTS(Y4, AX) \
-	COUNTS(Y3, BX) \
-	LEAL (BX)(AX*2), AX \
-	COUNTS(Y1, BX) \
-	COUNTS(Y0, DX) \
-	LEAL (DX)(BX*2), BX \
-	LEAL (BX)(AX*4), AX \
-	ADDQ AX, R
+// pseudo-transpose the 4x8 bit matrices in Y.  Transforms
+// Y = D7D6D5D4 D3D2D1D0 C7C6C5C4 C3C2C1C0 B7B6B5B4 B3B2B1B0 A7A6A5A4 A3A2A1A0
+// to  D7C7B7A7 D3C3B3A3 D6C6B6A6 D2C2B2A2 D5C5B5A5 D1C1B1A1 D4C4B4A4 D0C0B0A0
+// requires magic constants 0x00aa00aa in Y14 and 0x0000cccc in Y15
+#define TRANSPOSE(Y) \
+	BITPERMUTE(Y, Y14, $7) \
+	BITPERMUTE(Y, Y15, $14)
+
+// swap the bits of Y selected by mask M with those S bits to the left
+#define BITPERMUTE(Y, M, S) \
+	VPSRLD S, Y, Y12 \
+	VPXOR Y, Y12, Y12 \
+	VPAND Y12, M, Y12 \
+	VPXOR Y, Y12, Y \
+	VPSLLD S, Y12, Y12 \
+	VPXOR Y, Y12, Y
 
 // func count8avx2(counts *[8]int, buf []byte)
 TEXT ·count8avx2(SB),NOSPLIT,$0-32
@@ -92,7 +96,8 @@ vec15:	VMOVDQU 0*32(SI), Y0		// load 480 bytes from buf into Y0--Y14
 	CSA(Y12, Y13, Y14, Y15)
 
 	ADDQ $15*32, SI
-#define D	75
+
+#define D	50
 	PREFETCHT0 (D+ 0)*32(SI)
 	PREFETCHT0 (D+ 2)*32(SI)
 	PREFETCHT0 (D+ 4)*32(SI)
@@ -109,16 +114,77 @@ vec15:	VMOVDQU 0*32(SI), Y0		// load 480 bytes from buf into Y0--Y14
 	CSA(Y1, Y9, Y13, Y15)
 	CSA(Y3, Y4, Y9, Y15)
 
+	VPBROADCASTD magic<>+0(SB), Y14	// for TRANSPOSE
+	VPBROADCASTD magic<>+4(SB), Y15	// for TRANSPOSE
+	VPBROADCASTD magic<>+8(SB), Y13	// low nibbles
+
 	// Y4:Y3:Y1:Y0 = Y0+Y1+...+Y14
 
-	ACCUMS(R15)
-	ACCUMS(R14)
-	ACCUMS(R13)
-	ACCUMS(R12)
-	ACCUMS(R11)
-	ACCUMS(R10)
-	ACCUMS(R9)
-	ACCUM(R8)
+	// shuffle registers such that Y3:Y2:Y1:Y0 contains dwords
+	// of the form 0xDDCCBBAA
+	VPUNPCKLBW Y4, Y3, Y6		// Y6 = DDCCDDCC (lo)
+	VPUNPCKHBW Y4, Y3, Y7		// Y7 = DDCCDDCC (hi)
+	VPUNPCKLBW Y1, Y0, Y4		// Y4 = BBAABBAA (lo)
+	VPUNPCKHBW Y1, Y0, Y5		// Y5 = BBAABBAA (hi)
+	VPUNPCKLWD Y6, Y4, Y0		// Y0 = DDCCBBAA (0)
+	VPUNPCKHWD Y6, Y4, Y1		// Y1 = DDCCBBAA (1)
+	VPUNPCKLWD Y7, Y5, Y2		// Y2 = DDCCBBAA (2)
+	VPUNPCKHWD Y7, Y5, Y3		// Y3 = DDCCBBAA (3)
+
+	// pseudo-transpose the 8x4 bit matrix in each dword
+	TRANSPOSE(Y0)
+	TRANSPOSE(Y1)
+	TRANSPOSE(Y2)
+	TRANSPOSE(Y3)
+
+	// pull out the low halves and sum them up into Y4
+	// then right-shift high halves for further summing
+	VPAND Y0, Y13, Y4
+	VPSRLD $4, Y0, Y0
+	VPAND Y1, Y13, Y5
+	VPSRLD $4, Y1, Y1
+	VPADDB Y4, Y5, Y4
+	VPAND Y2, Y13, Y5
+	VPSRLD $4, Y2, Y2
+	VPADDB Y4, Y5, Y4
+	VPAND Y3, Y13, Y5
+	VPSRLD $4, Y3, Y3
+	VPADDB Y4, Y5, Y4
+
+	// pull out high halves and sum them up into Y0
+	VPAND Y0, Y13, Y0
+	VPAND Y1, Y13, Y1
+	VPADDB Y0, Y1, Y0
+	VPAND Y2, Y13, Y2
+	VPAND Y3, Y13, Y3
+	VPADDB Y2, Y3, Y2
+	VPADDB Y0, Y2, Y0
+
+	// join the two together with horizontal sums
+	VPHADDD Y0, Y4, Y0	// Y0 = 7654:7654:3210:3210:7654:7654:3210:3210
+	VEXTRACTI128 $1, Y0, X1	// X1 =                     7654:7654:3210:3210
+	VPHADDD X0, X1, X0	// X0 =                     7654:3210:7654:3210
+	VPMOVZXBW X0, Y0	// Y0 =                     7654:3210:7654:3210
+	VEXTRACTI128 $1, Y0, X1	// X1 =                               7654:3210
+	VPADDW X0, X1, X0	// X0 =                               7654:3210
+
+	// add to counters
+	VPEXTRW $0, X0, AX
+	ADDQ AX, R8
+	VPEXTRW $1, X0, AX
+	ADDQ AX, R9
+	VPEXTRW $2, X0, AX
+	ADDQ AX, R10
+	VPEXTRW $3, X0, AX
+	ADDQ AX, R11
+	VPEXTRW $4, X0, AX
+	ADDQ AX, R12
+	VPEXTRW $5, X0, AX
+	ADDQ AX, R13
+	VPEXTRW $6, X0, AX
+	ADDQ AX, R14
+	VPEXTRW $7, X0, AX
+	ADDQ AX, R15
 
 	SUBQ $15*32, CX
 	JGE vec15			// repeat as long as bytes are left
