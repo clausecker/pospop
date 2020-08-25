22cf965ebe19fa8f56973ddf7172e1d0689a43ea
Robert Clausecker <fuz@fuz.su>
2020-08-25 18:43:41 +0200

    update count16avx2 to use matrix transposition approach
    Now achieves 29 GB/s on my Skylake box

--- count16avx2_amd64.s
+++ count16avx2_amd64.s
@@ -27,36 +27,28 @@
 	COUNT(Y, R1, R2) \
 	VPADDB Y, Y, Y
 
-// count the number of MSB set in Y4:Y3:Y1:Y0
-// and accumulate into R1, R2
-#define ACCUM(R1, R2) \
-	COUNT(Y4, AX, BX) \
-	COUNT(Y3, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	COUNT(Y1, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	COUNT(Y0, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	ADDQ AX, R1 \
-	ADDQ BX, R2 \
+// magic transposition constants
+DATA magic<>+0(SB)/4, $0x00aa00aa
+DATA magic<>+4(SB)/4, $0x0000cccc
+DATA magic<>+8(SB)/4, $0x0f0f0f0f
+GLOBL magic<>(SB), RODATA|NOPTR, $12
 
-// same as ACCUM, but use COUNTS instead of COUNT
-#define ACCUMS(R1, R2) \
-	COUNTS(Y4, AX, BX) \
-	COUNTS(Y3, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	COUNTS(Y1, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	COUNTS(Y0, DX, BP) \
-	LEAL (DX)(AX*2), AX \
-	LEAL (BP)(BX*2), BX \
-	ADDQ AX, R1 \
-	ADDQ BX, R2 \
+// pseudo-transpose the 4x8 bit matrices in Y.  Transforms
+// Y = D7D6D5D4 D3D2D1D0 C7C6C5C4 C3C2C1C0 B7B6B5B4 B3B2B1B0 A7A6A5A4 A3A2A1A0
+// to  D7C7B7A7 D3C3B3A3 D6C6B6A6 D2C2B2A2 D5C5B5A5 D1C1B1A1 D4C4B4A4 D0C0B0A0
+// requires magic constants 0x00aa00aa in Y14 and 0x0000cccc in Y15
+#define TRANSPOSE(Y) \
+	BITPERMUTE(Y, Y14, $7) \
+	BITPERMUTE(Y, Y15, $14)
+
+// swap the bits of Y selected by mask M with those S bits to the left
+#define BITPERMUTE(Y, M, S) \
+	VPSRLD S, Y, Y12 \
+	VPXOR Y, Y12, Y12 \
+	VPAND Y12, M, Y12 \
+	VPXOR Y, Y12, Y \
+	VPSLLD S, Y12, Y12 \
+	VPXOR Y, Y12, Y
 
 // process two bits by shifting right AX and then adding
 // the carry to A and B
@@ -71,16 +63,6 @@ TEXT ·count16avx2(SB),NOSPLIT,$0-32
 	MOVQ counts+0(FP), DI
 	MOVQ buf_base+8(FP), SI		// SI = &buf[0]
 	MOVQ buf_len+16(FP), CX		// CX = len(buf)
-
-	// load every other counter into register R8--R15
-	MOVQ 8*0(DI), R8
-	MOVQ 8*2(DI), R9
-	MOVQ 8*4(DI), R10
-	MOVQ 8*6(DI), R11
-	MOVQ 8*8(DI), R12
-	MOVQ 8*10(DI), R13
-	MOVQ 8*12(DI), R14
-	MOVQ 8*14(DI), R15
 
 	SUBQ $15*32/2, CX			// pre-decrement CX
 	JL end15
@@ -111,7 +93,7 @@ vec15:	VMOVDQU 0*32(SI), Y0		// load 480 bytes from buf into Y0--Y14
 	CSA(Y12, Y13, Y14, Y15)
 
 	ADDQ $15*32, SI
-#define D	50
+#define D	75
 	PREFETCHT0 (D+ 0)*32(SI)
 	PREFETCHT0 (D+ 2)*32(SI)
 	PREFETCHT0 (D+ 4)*32(SI)
@@ -130,20 +112,100 @@ vec15:	VMOVDQU 0*32(SI), Y0		// load 480 bytes from buf into Y0--Y14
 
 	// Y4:Y3:Y1:Y0 = Y0+Y1+...+Y14
 
-	ACCUMS(8*7(DI), 8*15(DI))
-	ACCUMS(R11, R15)
-	ACCUMS(8*5(DI), 8*13(DI))
-	ACCUMS(R10, R14)
-	ACCUMS(8*3(DI), 8*11(DI))
-	ACCUMS(R9, R13)
-	ACCUMS(8*1(DI), 8*9(DI))
-	ACCUM(R8, R12)
+	VPBROADCASTD magic<>+0(SB), Y14	// for TRANSPOSE
+	VPBROADCASTD magic<>+4(SB), Y15	// for TRANSPOSE
+	VPBROADCASTD magic<>+8(SB), Y13	// low nibbles
+
+	// Y4:Y3:Y1:Y0 = Y0+Y1+...+Y14
+
+	// shuffle registers such that Y3:Y2:Y1:Y0 contains dwords
+	// of the form 0xDDCCBBAA
+	VPUNPCKLBW Y4, Y3, Y6		// Y6 = DDCCDDCC (lo)
+	VPUNPCKHBW Y4, Y3, Y7		// Y7 = DDCCDDCC (hi)
+	VPUNPCKLBW Y1, Y0, Y4		// Y4 = BBAABBAA (lo)
+	VPUNPCKHBW Y1, Y0, Y5		// Y5 = BBAABBAA (hi)
+	VPUNPCKLWD Y6, Y4, Y0		// Y0 = DDCCBBAA (0)
+	VPUNPCKHWD Y6, Y4, Y1		// Y1 = DDCCBBAA (1)
+	VPUNPCKLWD Y7, Y5, Y2		// Y2 = DDCCBBAA (2)
+	VPUNPCKHWD Y7, Y5, Y3		// Y3 = DDCCBBAA (3)
+
+	// pseudo-transpose the 8x4 bit matrix in each dword
+	TRANSPOSE(Y0)
+	TRANSPOSE(Y1)
+	TRANSPOSE(Y2)
+	TRANSPOSE(Y3)
+
+	// pre-load counters into some spare registers
+	VMOVDQU 0*32(DI), Y8
+	VMOVDQU 1*32(DI), Y9
+	VMOVDQU 2*32(DI), Y10
+	VMOVDQU 3*32(DI), Y11
+
+	// pull out low nibbles from matrices
+	VPAND Y0, Y13, Y4
+	VPSRLD $4, Y0, Y0
+	VPAND Y1, Y13, Y5
+	VPSRLD $4, Y1, Y1
+	VPADDB Y4, Y5, Y4
+	VPAND Y2, Y13, Y6
+	VPSRLD $4, Y2, Y2
+	VPAND Y3, Y13, Y7
+	VPSRLD $4, Y3, Y3
+	VPADDB Y6, Y7, Y6
+	VPADDB Y4, Y6, Y6	// Y6 = ba98:3210:ba98:3210:ba98:3210:ba98:3210
+
+	// pull out high nibbles from matrices
+	VPAND Y0, Y13, Y0
+	VPAND Y1, Y13, Y1
+	VPADDB Y0, Y1, Y4
+	VPAND Y2, Y13, Y2
+	VPAND Y3, Y13, Y3
+	VPADDB Y2, Y3, Y5
+	VPADDB Y4, Y5, Y4	// Y4 = fedc:7654:fedc:7654:fedc:7654:fedc:7654
+
+	VPUNPCKLDQ Y4, Y6, Y0
+	VPUNPCKHDQ Y4, Y6, Y1
+	VPADDB Y0, Y1, Y0	// Y0 = fedc:ba98:7654:3210:fedc:ba98:7654:3210
+	VEXTRACTI128 $1, Y0, X1	// X1 =                     fedc:ba98:7654:3210
+	VPADDB X1, X0, X0	// X0 =                     fedc:ba98:7654:3210
+
+        // add to counters
+	VPMOVZXBQ X0, Y1
+	VPSRLDQ $4, X0, X0
+	VPADDQ Y1, Y8, Y8
+	VMOVDQU Y8, 0*32(DI)
+
+	VPMOVZXBQ X0, Y1
+	VPSRLDQ $4, X0, X0
+	VPADDQ Y1, Y9, Y9
+	VMOVDQU Y9, 1*32(DI)
+
+	VPMOVZXBQ X0, Y1
+	VPSRLDQ $4, X0, X0
+	VPADDQ Y1, Y10, Y10
+	VMOVDQU Y10, 2*32(DI)
+
+	VPMOVZXBQ X0, Y1
+	VPSRLDQ $4, X0, X0
+	VPADDQ Y1, Y11, Y11
+	VMOVDQU Y11, 3*32(DI)
 
 	SUBQ $15*(32/2), CX
 	JGE vec15			// repeat as long as bytes are left
 
 end15:	SUBQ $-14*(32/2), CX		// undo last subtraction and
 					// pre-subtract 32 bit from CX
+
+	// load every other counter into register R8--R15
+	MOVQ 8*0(DI), R8
+	MOVQ 8*2(DI), R9
+	MOVQ 8*4(DI), R10
+	MOVQ 8*6(DI), R11
+	MOVQ 8*8(DI), R12
+	MOVQ 8*10(DI), R13
+	MOVQ 8*12(DI), R14
+	MOVQ 8*14(DI), R15
+
 	JL end1
 
 vec1:	VMOVDQU (SI), Y0		// load 32 bytes from buf
