19f1a279dd95063b7e09dc222c98b780a088c0c3
Robert Clausecker <fuz@fuz.su>
2020-08-28 16:39:07 +0200

    add benchmarks for Count32 and Count64

--- bench_test.go
+++ bench_test.go
@@ -97,3 +97,89 @@ func BenchmarkCount16(b *testing.B) {
 		})
 	}
 }
+
+// benchmark a count32 implementation
+func benchmarkCount32(b *testing.B, buf []uint32, lengths []int, count32 func(*[32]int, []uint32)) {
+	for _, l := range lengths {
+		b.Run(strconv.Itoa(l), func(b *testing.B) {
+			var counts [32]int
+			testbuf := buf[:l]
+			b.SetBytes(int64(l) * 4)
+			for i := 0; i < b.N; i++ {
+				count32(&counts, testbuf)
+			}
+		})
+	}
+}
+
+// benchmark all Count32 implementations
+func BenchmarkCount32(b *testing.B) {
+	funcs := count32funcs
+	lengths := benchmarkLengths
+
+	// short benchmark: only test the implementation
+	// actually used and keep it to one size
+	if testing.Short() {
+		funcs = []count32impl{{Count32, "dispatch", true}}
+		lengths = benchmarkLengthsShort
+	}
+
+	maxlen := lengths[len(lengths)-1]
+	buf := make([]uint32, maxlen)
+	for i := range buf {
+		buf[i] = uint32(rand.Int63())
+	}
+
+	for _, impl := range funcs {
+		b.Run(impl.name, func(bb *testing.B) {
+			if !impl.available {
+				bb.SkipNow()
+			}
+
+			benchmarkCount32(bb, buf, lengths, impl.count32)
+		})
+	}
+}
+
+// benchmark a count64 implementation
+func benchmarkCount64(b *testing.B, buf []uint64, lengths []int, count64 func(*[64]int, []uint64)) {
+	for _, l := range lengths {
+		b.Run(strconv.Itoa(l), func(b *testing.B) {
+			var counts [64]int
+			testbuf := buf[:l]
+			b.SetBytes(int64(l) * 8)
+			for i := 0; i < b.N; i++ {
+				count64(&counts, testbuf)
+			}
+		})
+	}
+}
+
+// benchmark all Count64 implementations
+func BenchmarkCount64(b *testing.B) {
+	funcs := count64funcs
+	lengths := benchmarkLengths
+
+	// short benchmark: only test the implementation
+	// actually used and keep it to one size
+	if testing.Short() {
+		funcs = []count64impl{{Count64, "dispatch", true}}
+		lengths = benchmarkLengthsShort
+	}
+
+	maxlen := lengths[len(lengths)-1]
+	buf := make([]uint64, maxlen)
+	for i := range buf {
+		buf[i] = rand.Uint64()
+	}
+
+	for _, impl := range funcs {
+		b.Run(impl.name, func(bb *testing.B) {
+			if !impl.available {
+				bb.SkipNow()
+			}
+
+			benchmarkCount64(bb, buf, lengths, impl.count64)
+		})
+	}
+}
