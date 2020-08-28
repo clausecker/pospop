88abec24d4b1926c1bdbc34fc35bd39c0dd16392
Klaus Post <klauspost@gmail.com>
2020-08-28 11:42:26 +0200

    Speed up Go implementation
    Unroll inner loop and postpone shifting.
    
    ```
    benchmark                                 old ns/op      new ns/op     delta
    BenchmarkCount8/generic/1000-32           5049           2008          -60.23%
    BenchmarkCount8/generic/10000-32          50975          20084         -60.60%
    BenchmarkCount8/generic/100000-32         510629         201260        -60.59%
    BenchmarkCount8/generic/1000000-32        5186954        2013423       -61.18%
    BenchmarkCount8/generic/10000000-32       51857148       20017886      -61.40%
    BenchmarkCount8/generic/100000000-32      515500200      200000200     -61.20%
    BenchmarkCount16/generic/1000-32          14440          3793          -73.73%
    BenchmarkCount16/generic/10000-32         145768         38286         -73.73%
    BenchmarkCount16/generic/100000-32        1451220        381340        -73.72%
    BenchmarkCount16/generic/1000000-32       15645678       3799999       -75.71%
    BenchmarkCount16/generic/10000000-32      154999986      38032245      -75.46%
    BenchmarkCount16/generic/100000000-32     1544998200     379333367     -75.45%
    
    benchmark                                 old MB/s     new MB/s     speedup
    BenchmarkCount8/generic/1000-32           198.06       497.94       2.51x
    BenchmarkCount8/generic/10000-32          196.17       497.92       2.54x
    BenchmarkCount8/generic/100000-32         195.84       496.87       2.54x
    BenchmarkCount8/generic/1000000-32        192.79       496.67       2.58x
    BenchmarkCount8/generic/10000000-32       192.84       499.55       2.59x
    BenchmarkCount8/generic/100000000-32      193.99       500.00       2.58x
    BenchmarkCount16/generic/1000-32          138.51       527.22       3.81x
    BenchmarkCount16/generic/10000-32         137.20       522.38       3.81x
    BenchmarkCount16/generic/100000-32        137.82       524.47       3.81x
    BenchmarkCount16/generic/1000000-32       127.83       526.32       4.12x
    BenchmarkCount16/generic/10000000-32      129.03       525.87       4.08x
    BenchmarkCount16/generic/100000000-32     129.45       527.24       4.07x
    ```

--- generic.go
+++ generic.go
@@ -4,6 +4,28 @@ package pospop
 
 // count8 generic implementation
 func count8generic(counts *[8]int, buf []uint8) {
+	if uint64(len(buf)) >= 1<<(64-8) {
+		// Use fallback if we risk overflowing
+		count8safe(counts, buf)
+		return
+	}
+	var tmp [8]uint64
+	for _, v := range buf {
+		tmp[0] += uint64(v & 1)
+		tmp[1] += uint64(v & (1 << 1))
+		tmp[2] += uint64(v & (1 << 2))
+		tmp[3] += uint64(v & (1 << 3))
+		tmp[4] += uint64(v & (1 << 4))
+		tmp[5] += uint64(v & (1 << 5))
+		tmp[6] += uint64(v & (1 << 6))
+		tmp[7] += uint64(v & (1 << 7))
+	}
+	for i, v := range tmp[:] {
+		(*counts)[i] += int(v >> i)
+	}
+}
+
+func count8safe(counts *[8]int, buf []uint8) {
 	for i := 0; i < len(buf); i++ {
 		for j := 0; j < 8; j++ {
 			(*counts)[j] += int(buf[i] >> j & 1)
@@ -13,6 +35,37 @@ func count8generic(counts *[8]int, buf []uint8) {
 
 // count16 generic implementation
 func count16generic(counts *[16]int, buf []uint16) {
+	if uint64(len(buf)) >= 1<<(64-16) {
+		// Use fallback if we risk overflowing
+		count16safe(counts, buf)
+		return
+	}
+	var tmp [16]uint64
+	for _, v := range buf {
+		tmp[0] += uint64(v & 1)
+		tmp[1] += uint64(v & (1 << 1))
+		tmp[2] += uint64(v & (1 << 2))
+		tmp[3] += uint64(v & (1 << 3))
+		tmp[4] += uint64(v & (1 << 4))
+		tmp[5] += uint64(v & (1 << 5))
+		tmp[6] += uint64(v & (1 << 6))
+		tmp[7] += uint64(v & (1 << 7))
+
+		tmp[8] += uint64(v & (1 << 8))
+		tmp[9] += uint64(v & (1 << 9))
+		tmp[10] += uint64(v & (1 << 10))
+		tmp[11] += uint64(v & (1 << 11))
+		tmp[12] += uint64(v & (1 << 12))
+		tmp[13] += uint64(v & (1 << 13))
+		tmp[14] += uint64(v & (1 << 14))
+		tmp[15] += uint64(v & (1 << 15))
+	}
+	for i, v := range tmp[:] {
+		(*counts)[i] += int(v >> i)
+	}
+}
+
+func count16safe(counts *[16]int, buf []uint16) {
 	for i := 0; i < len(buf); i++ {
 		for j := 0; j < 16; j++ {
 			(*counts)[j] += int(buf[i] >> j & 1)
