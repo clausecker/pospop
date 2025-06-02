a9a72df0d1a3eb845125116c1d513116e604cc8d
Robert Clausecker <fuz@fuz.su>
2025-06-02 15:20:53 +0200

    dummy_arm64_test.go: remove unused sink variables

--- dummy_arm64_test.go
+++ dummy_arm64_test.go
@@ -2,15 +2,16 @@
 
 package pospop
 
-var count8dummy = []count8impl{{dummyCount8, "dummy", true}}
+import "golang.org/x/sys/cpu"
+
+var count8dummy = []count8impl{
+	{dummyCount8avx512, "dummyAvx512", cpu.X86.HasAVX512},
+	{dummyCount8avx, "dummyAvx", cpu.X86.HasAVX && !cpu.X86.HasAVX512},
+	{dummyCount8sse, "dummySse", !cpu.X86.HasAVX}, 
+}
 var count16dummy = []count16impl{{dummyCount16, "dummy", true}}
 var count32dummy = []count32impl{{dummyCount32, "dummy", true}}
 var count64dummy = []count64impl{{dummyCount64, "dummy", true}}
-
-var sink8 uint8
-var sink16 uint16
-var sink32 uint32
-var sink64 uint64
 
 // dummy Count8 implementation that performs no work
 func dummyCount8(counts *[8]int, buf []uint8)
