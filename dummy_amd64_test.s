226e6e9a7e09ce025543c5a534b401b862a0f00f
Robert Clausecker <fuz@fuz.su>
2025-06-04 12:59:28 +0200

    dummy_*_test: accumulate results
    There is some suspicion that the dummy baseline is too fast
    because I don't use the results.  Make sure the results are
    accumulated (by exclusive-or) so the CPU cannot possibly
    optimise out the loads.

--- dummy_amd64_test.s
+++ dummy_amd64_test.s
@@ -7,11 +7,12 @@ TEXT countdummyavx512<>(SB), NOSPLIT, $0-0
 	ANDQ $63, AX		// offset of buffer start from 64 byte alignment
 	ANDQ $~63, SI		// align input buffer to 64 bytes
 	MOVQ $63(CX)(AX*1), CX	// add offset and 63 to buffer length
+	VXORPS Z4, Z4, Z4
 	TESTQ $64, CX		// is there an odd number of 64 byte lanes to process?
 	JEQ even64
 
 	// odd number of 64 byte lanes, process first lane
-	VMOVAPS (SI), Z0
+	VMOVAPS (SI), Z4
 	ADDQ $64, SI
 	SUBQ $64, CX
 
@@ -21,6 +22,8 @@ even64:	TESTQ $128, CX		// is there an odd number of 128 byte lanes to process?
 	// odd number of 128 byte lanes, process first lane
 	VMOVAPS (SI), Z0
 	VMOVAPS 64(SI), Z1
+	VXORPS Z0, Z1, Z1	// reduce into Z4
+	VXORPS Z1, Z4, Z4
 	ADDQ $128, SI
 	SUBQ $128, CX
 
@@ -32,6 +35,10 @@ loop:	VMOVAPS 64*0(SI), Z0
 	VMOVAPS 64*1(SI), Z1
 	VMOVAPS 64*2(SI), Z2
 	VMOVAPS 64*3(SI), Z3
+	VXORPS Z0, Z1, Z1	// reduce into Z4
+	VXORPS Z2, Z3, Z3
+	VXORPS Z1, Z3, Z3
+	VXORPS Z3, Z4, Z4
 	ADDQ $256, SI
 	SUBQ $256, CX
 	JNE loop
@@ -45,11 +52,12 @@ TEXT countdummyavx<>(SB), NOSPLIT, $0-0
 	ANDQ $31, AX		// offset of buffer start from 32 byte alignment
 	ANDQ $~31, SI		// align input buffer to 32 bytes
 	MOVQ $31(CX)(AX*1), CX	// add offset and 31 to buffer length
+	VXORPS Y4, Y4, Y4
 	TESTQ $32, CX		// is there an odd number of 32 byte lanes to process?
 	JEQ even32
 
 	// odd number of 32 byte lanes, process first lane
-	VMOVAPS (SI), Y0
+	VMOVAPS (SI), Y4
 	ADDQ $32, SI
 	SUBQ $32, CX
 
@@ -59,6 +67,8 @@ even32:	TESTQ $64, CX		// is there an odd number of 64 byte lanes to process?
 	// odd number of 64 byte lanes, process first lane
 	VMOVAPS (SI), Y0
 	VMOVAPS 32(SI), Y1
+	VXORPS Y0, Y1, Y1
+	VXORPS Y1, Y4, Y4
 	ADDQ $64, SI
 	SUBQ $64, CX
 
@@ -69,6 +79,10 @@ loop:	VMOVAPS 32*0(SI), Y0
 	VMOVAPS 32*1(SI), Y1
 	VMOVAPS 32*2(SI), Y2
 	VMOVAPS 32*3(SI), Y3
+	VXORPS Y0, Y1, Y1
+	VXORPS Y2, Y3, Y3
+	VXORPS Y1, Y3, Y3
+	VXORPS Y3, Y4, Y4
 	ADDQ $128, SI
 	SUBQ $128, CX
 	JNE loop
@@ -82,11 +96,12 @@ TEXT countdummysse<>(SB), NOSPLIT, $0-0
 	ANDQ $15, AX		// offset of buffer start from 16 byte alignment
 	ANDQ $~15, SI		// align input buffer to 16 bytes
 	MOVQ $15(CX)(AX*1), CX	// add offset and 15 to buffer length
+	XORPS X4, X4
 	TESTQ $16, CX		// is there an odd number of 16 byte lanes to process?
 	JEQ even16
 
 	// odd number of 16 byte lanes, process first lane
-	MOVAPS (SI), X0
+	MOVAPS (SI), X4
 	ADDQ $16, SI
 	SUBQ $16, CX
 
@@ -96,6 +111,8 @@ even16:	TESTQ $32, CX		// is there an odd number of 32 byte lanes to process?
 	// odd number of 32 byte lanes, process first lane
 	MOVAPS (SI), X0
 	MOVAPS 16(SI), X1
+	XORPS X0, X1
+	XORPS X1, X4
 	ADDQ $32, SI
 	SUBQ $32, CX
 
@@ -106,6 +123,10 @@ loop:	MOVAPS 16*0(SI), X0
 	MOVAPS 16*1(SI), X1
 	MOVAPS 16*2(SI), X2
 	MOVAPS 16*3(SI), X3
+	XORPS X0, X1
+	XORPS X2, X3
+	XORPS X1, X3
+	XORPS X3, X4
 	ADDQ $64, SI
 	SUBQ $64, CX
 	JNE loop
--- dummy_arm64_test.s
+++ dummy_arm64_test.s
@@ -8,18 +8,31 @@ TEXT countdummy<>(SB), NOSPLIT, $0-0
 	ADD R2, R3, R3		// add offset to buffer length
 
 	ADD $63, R3, R3		// round up to next multiple of 64 bytes
+	VEOR V8.B16, V8.B16, V8.B16
 	TST $64, R3		// is there an odd number of 64 byte lanes to process?
 	BEQ even
 
 	// odd number of lanes, process first lane
 	VLD1.P 4*16(R1), [V0.B16, V1.B16, V2.B16, V3.B16]
+	VEOR V0.B16, V1.B16, V1.B16
+	VEOR V2.B16, V3.B16, V3.B16
+	VEOR V1.B16, V3.B16, V3.B16
+	VEOR V3.B16, V8.B16, V8.B16
 	SUB $64, R3, R3
 
 even:	ANDS $~63, R3, R3	// truncate count to multiple of 64 bytes
 	BEQ end			// if now empty, exit
 
 loop:	VLD1.P 4*16(R1), [V0.B16, V1.B16, V2.B16, V3.B16]
-	VLD1.P 4*16(R1), [V0.B16, V1.B16, V2.B16, V3.B16]
+	VLD1.P 4*16(R1), [V4.B16, V5.B16, V6.B16, V7.B16]
+	VEOR V0.B16, V1.B16, V1.B16
+	VEOR V2.B16, V3.B16, V3.B16
+	VEOR V4.B16, V5.B16, V5.B16
+	VEOR V6.B16, V7.B16, V7.B16
+	VEOR V1.B16, V3.B16, V3.B16
+	VEOR V5.B16, V7.B16, V7.B16
+	VEOR V3.B16, V7.B16, V7.B16
+	VEOR V7.B16, V8.B16, V8.B16
 	SUBS $128, R3, R3
 	BNE loop
 
